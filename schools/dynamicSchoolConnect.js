2fdcdc9504e72df7cccd80eaa9002e377469b162
MaciejBaj <macie.baj@gmail.com>
2016-01-02 18:23:57 +0100

    comments to school content shownig

--- schools/dynamicSchoolConnect.js
+++ schools/dynamicSchoolConnect.js
@@ -7,13 +7,15 @@ var json2html = require('json2html');
 module.exports = function(app) {
   app.route('/subjects/school/:id').get(function(req, res) {
     var port = _.find(app.schools, {id: req.params.id});
+    console.log("found school at port ", port);
+
     if(!port) {
       res.status(404).send("no mongo connection can be found for school of id ", req.params.id);
       return;
     }
     var schoolOneConnection = mongoose.createConnection('mongodb://' + mongoDbAdressess.ministry + ',' + mongoDbAdressess.host + ':' + port + '/education?w=0&readPreference=secondary');
     var SubjectsCollection = schoolOneConnection.model('Subject', subjectModel);
-
+    console.log("connection exists", !!schoolOneConnection, "connection exists", !!SubjectsCollection);
     SubjectsCollection.find(function (err, subjects) {
       if (err) {
         return res.status(500).send(err);
